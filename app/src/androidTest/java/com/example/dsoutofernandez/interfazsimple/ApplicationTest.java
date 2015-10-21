b78608ded7cb1a034dda93528c95331d9a9bfcef
dsoutofernandez <dsoutofernandez@atanasoft222.danielcastelao.org>
2015-10-21 11:20:38 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/dsoutofernandez/interfazsimple/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.example.dsoutofernandez.interfazsimple;
+
+import android.app.Application;
+import android.content.Intent;
+import android.test.ApplicationTestCase;
+import android.view.View;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+
+
+
+}
+
